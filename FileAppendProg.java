aaefd96a60ab638834b73a377ff2205581a2dc21
PavithraBGowda <100990331+PavithraBGowda@users.noreply.github.com>
2022-03-06 21:59:18 +0530

    Add files via upload

---
+++ FileAppendProg.java
@@ -0,0 +1,29 @@
+package file_Write_Read_Append;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class FileAppendProg {
+
+	public static void main(String[] args) {
+		String path ="C:/Users/pavit/Desktop/Simplilearn/FSD_Phase_1/employee1.txt";
+		String text = " taking training from simplelearn ";
+		try
+		{
+			FileWriter f = new FileWriter(path,true);
+			f.write(text);
+			f.close();
+			System.out.println("Append operation on file was done Successfully..");
+		}
+		catch(IOException e)
+		{
+			 System.out.println(e);
+			 
+		} 
+		
+
+
+	}
+
+}

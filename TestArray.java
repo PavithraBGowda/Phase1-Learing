a3ffcf3590ed0c491d487561479467e83193b82c
PavithraBGowda <100990331+PavithraBGowda@users.noreply.github.com>
2022-03-06 22:01:40 +0530

    Add files via upload

---
+++ TestArray.java
@@ -0,0 +1,15 @@
+package AssignmemtFeb23;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		int a[] = {7,6,3,4};
+		int b[] = {4,9,7,2};
+		int c[] = new int[4];
+		for (int i=0;i<4;i++)
+		{
+			System.out.println(a[i]+b[i]);
+		}
+	}
+
+}
